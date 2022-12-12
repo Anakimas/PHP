6037f66c3c4a8fb8aab152db89259a63f47e7f86
John Doe <johndoe@example.com>
2022-12-12 20:31:57 +0200

    failas

---
+++ antrapaskaita.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+//$variableToDebug = 15;
+//var_dump(['Hello', 'Learning', 'Amazing', 'CodeAcademy', 'Php', 'Git']);
+//
+//var_dump((int)false);
+//var_dump((int)true);
+//var_dump((int)'2022 year');
+//
+//var_dump((bool)150);
+//var_dump((bool)[]);
+//var_dump((bool)'Year - 2022');
+//
+//
+//$variable7 = 145;
+//$variable8 = '145';
+//$variable9 = '150';
+//$variable10 = 150;
+//
+//var_dump((!$variable7 || $variable8));
+//var_dump((!$variable9 && $variable10));
+
+//$i= 143*44;
+//var_dump($i);
+//$i= 567/12;
+//var_dump($i);
+//$i= 578%55;
+//var_dump($i);
+//$i= 6**5;
+//var_dump($i);
+//$i= 5;
+//$i--;
+//var_dump($i);
+//$i= 6;
+//$i++;
+//var_dump($i);
+
+
+//$i= 56 > 77;
+//var_dump($i);
+//$i= 190 <= 230;
+//var_dump($i);
+//$i= 230 == '230';
+//var_dump($i);
+//$i= 45 != '45';
+//var_dump($i);
+
+////$variable = 10 ;
+//var_dump($variable);
+//var_dump(isset($variable));
+
+
---
+++ treciapaskaita.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+//$i= 987 + 545 - 32 * 94;
+//var_dump($i);
+//$i= 32 * 32 * 32 + 18;
+//var_dump($i);
+//$i= 120/4/3;
+//var_dump($i);
+//$i= 187%5;
+//var_dump($i);
+//$numA=3;
+//$numA++;
+//$numA++;
+//$numA++;
+//var_dump($numA);
+//$numA=12;
+//$numA--;
+//$numA--;
+//$numA--;
+//$numA--;
+//var_dump($numA);
+
+//for ($i =0; $i<11; $i++){
+//    echo $i. PHP_EOL;
+//}
+
+//$i = 15;
+//while ($i >= 3) {
+//    echo $i. PHP_EOL;
+//    $i--;
+//}
+
+//for( $i=1;$i<=20;$i+=2){
+//    echo $i. PHP_EOL;
+//}
+
+//for($i = 1; $i <= 20; $i++)
+//{
+//
+//    if( ($i%3) == 0 )
+//    {
+//        echo $i. PHP_EOL;
+//    }
+//}
+//return 0;
+
+
+for($i = 1; $i <= 20; $i++)
+{
+
+    if( ($i%3) == 0 && ($i%5) == 0)
+    {
+        echo $i. PHP_EOL;
+    }
+}
+
+
+
+
+
